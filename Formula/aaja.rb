8fa00a8da835fbb62c14af17425e38767183b801
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2020-06-30 11:18:05 +0545

    Update checksum

--- Formula/aaja.rb
+++ Formula/aaja.rb
@@ -2,7 +2,7 @@ class Aaja < Formula
     desc "Lightweight Nepali calendar package written in go"
     homepage "https://github.com/rabinpoudyal/aaja"
     url "https://github.com/rabinpoudyal/aaja/releases/latest/download/aaja-mac.tar.gz"
-    sha256 "27077822fcec1a5200db590b751ff135eefe4c2a2613d7e948a139a13deb7007"
+    sha256 "46a45dc8cf7bc8f680f599eac33818165cd26fde90caaa344ded0767f4a1c606"
     version "1.0.0"
   
     def install
